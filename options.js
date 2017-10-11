51bfada4004e7bb41b2e750bb0af9063b9cabd3a
C222 <C222@users.noreply.github.com>
2017-10-11 16:46:47 -0700

    Update options.js

--- options.js
+++ options.js
@@ -4,6 +4,14 @@ function PickOneFactory(the_list) {
   };
 }
 
+function IntegerFactory(min, max) {
+  return function() {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+}
+
 options = {};
 
-options['Team Size'] = function() { PickOneFactory(["1P", "2P", "3P", "4P"]); };
+options['Team Size'] = {rule: function() { return PickOneFactory(["2P", "3P", "4P"])(); }};
+options['Zombie'] = {rule: function() { return "&#x2714;"; }};
+options['DBNO'] = {rule: function() { return "&#x2718;"; }};
